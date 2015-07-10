873801d40d6f196c8e45643907f00d9b0214478f
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-07-10 15:09:30 -0700

    implement has_one_through

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,41 +10,85 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    "#{class_name.downcase}s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.capitalize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] ||
+                     "#{self_class_name.downcase}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.singularize.capitalize
   end
 end
 
 module Associatable
   # Phase IIIb
-  def belongs_to(name, options = {})
-    # ...
+  def belongs_to(name, options_hash = {})
+    options = BelongsToOptions.new(name, options_hash)
+    assoc_options[name] = options
+
+    define_method(name) do
+      foreign_key = options.foreign_key
+      primary_key = options.primary_key
+      other_table_name = options.table_name
+
+      results = DBConnection.execute(<<-SQL, self.id)
+        SELECT
+          o.*
+        FROM
+          #{self.class.table_name} s
+        JOIN
+          #{other_table_name} o ON o.#{primary_key} = s.#{foreign_key}
+        WHERE
+          s.id = ?
+      SQL
+
+      options.model_class.parse_all(results).first
+    end
   end
 
-  def has_many(name, options = {})
-    # ...
+  def has_many(name, options_hash = {})
+    options = HasManyOptions.new(name, self.name, options_hash)
+
+    define_method(name) do
+      foreign_key = options.foreign_key
+      primary_key = options.primary_key
+      other_table_name = options.table_name
+
+      results = DBConnection.execute(<<-SQL, self.id)
+        SELECT
+          o.*
+        FROM
+          #{self.class.table_name} s
+        JOIN
+          #{other_table_name} o ON s.#{primary_key} = o.#{foreign_key}
+        WHERE
+          s.id = ?
+      SQL
+
+      options.model_class.parse_all(results)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,28 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      result = DBConnection.execute(<<-SQL, self.id)
+        SELECT
+          source.*
+        FROM
+          #{self.class.table_name} self
+        JOIN
+          #{through_options.table_name} through
+        ON
+          through.id = self.#{through_options.foreign_key}
+        JOIN
+          #{source_options.table_name} source
+        ON
+          source.id = through.#{source_options.foreign_key}
+        WHERE
+          self.id = ?
+      SQL
+
+      source_options.model_class.parse_all(result).first
+    end
   end
 end
