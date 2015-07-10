1387301e3dfffc86c10f4c94c8508cc1d5a78a66
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-07-10 13:30:27 -0700

    add where method

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -1,5 +1,6 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
@@ -12,16 +13,14 @@ class SQLObject
         #{table_name}
     SQL
 
-    @attributes = {}
-
     columns.each do |column|
-      define_method(column) { @attributes[column] }
+      define_method(column) { attributes[column] }
 
       define_method("#{column}=") do |value|
-        @attributes ||= {}
-        @attributes[column] = value
+        attributes[column] = value
       end
     end
+    columns
   end
 
 
@@ -37,38 +36,78 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+    parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |params| new(params) }
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        id = ?
+    SQL
+    return nil if result.empty?
+    parse_all(result).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      unless self.class.columns.include?(attr_name.to_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+      send("#{attr_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    attributes.values
   end
 
   def insert
-    # ...
+    cols = self.class.columns.drop(1)
+    col_names = cols.map(&:to_s).join(', ')
+    question_marks = (['?'] * cols.length).join(', ')
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+    self
   end
 
   def update
-    # ...
+    cols = self.class.columns.drop(1)
+    assignments = cols.map { |attr_name| "#{attr_name} = ?"}.join(', ')
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1), self.id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{assignments}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,19 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    conditions = params.map { |attr_name, _| "#{attr_name} = ?" }.join(' AND ')
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{conditions}
+    SQL
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
